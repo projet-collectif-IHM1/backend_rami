e8f17989dd4af3a0f188ef13df27d31c601f8c50
ramii845 <rami.elfrikha@gmail.com>
2025-03-26 15:23:56 +0100

    Commit new version7

--- main.py
+++ main.py
@@ -307,12 +307,20 @@ async def get_chambre_by_id(chambre_id: str):
 @app.get("/chambres/", response_model=List[Chambre])
 async def get_all_chambres():
     # Récupérer toutes les chambres de la base de données
-    chambres = await db.chambres.find().to_list(100)  # Vous pouvez ajuster le nombre max de chambres ici
+    chambres = await db.chambres.find().to_list(100)
 
+    # Vérifier si aucune chambre n'est trouvée
     if not chambres:
         raise HTTPException(status_code=404, detail="Aucune chambre trouvée")
 
-    return chambres
+    # Convertir _id en string et l'ajouter en tant que 'id'
+    for chambre in chambres:
+        chambre["id"] = str(chambre["_id"])
+        del chambre["_id"]  # Supprimer _id original si nécessaire
+
+    return JSONResponse(status_code=200, content={"status_code": 200, "chambres": chambres})
+
+
 
 
 @app.put("/chambres/{chambre_id}", response_model=dict)
